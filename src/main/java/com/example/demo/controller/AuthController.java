7dbcbeb2ae91210a55048b87b511862e03ded109
villageworks <villageworks@gmail.com>
2024-08-06 04:42:34 +0900

    ログインから取引先一覧までの簡易的画面遷移

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -3,11 +3,21 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
-	@GetMapping("/customers")
+	
+	@PostMapping("/login")
+	public String login(
+			@RequestParam("email") String email, 
+			@RequestParam("password") String password,
+			Model model) {
+		return "companies";
+	}
+	
+	@GetMapping({"/", "/customers", "/customers/login"})
 	public String index(Model model) {
 		model.addAttribute("isLogged", null); // TODO: ログイン認証処理実装までの暫定措置
 		return "login";
